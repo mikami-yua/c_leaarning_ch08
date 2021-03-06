84ebf795d672035c33b6723861cc4c16c5c2d019
mikami-yua <lovejiaxi_1@163.com>
2021-03-06 19:54:43 +0800

    jieba装好了

--- 06vector存放内置数据类型.cpp
+++ 06vector存放内置数据类型.cpp
@@ -3,6 +3,9 @@ using namespace std;
 #include<vector>
 #include<algorithm>
 #include<string>
+#include<fstream>
+#include "cppjieba/Jieba.hpp"
+
 
 void myPrintVectotr(int val) {
 	cout << val << endl;
@@ -132,12 +135,52 @@ void test65() {
 	}
 }
 
+/*
+使用结巴需要
+拷贝include dept dict到项目目录
+在项目属性的include中设置设置include dept
+
+如果使用分词，如何防止客户端不规矩的分词（好像原来的版本也没有办法防止client不规矩的取token）
+*/
+
+void test66() {
+	char ch0[] = { "我喜欢吃苹果和apple banana" };
+	const char* const DICT_PATH = "dict/jieba.dict.utf8";
+	const char* const HMM_PATH = "dict/hmm_model.utf8";
+	const char* const USER_DICT_PATH = "dict/user.dict.utf8";
+	const char* const IDF_PATH = "dict/idf.utf8";
+	const char* const STOP_WORD_PATH = "dict/stop_words.utf8";
+	cout << "加载dict" << endl;
+	cppjieba::Jieba jieba(DICT_PATH,
+		HMM_PATH,
+		USER_DICT_PATH,
+		IDF_PATH,
+		STOP_WORD_PATH);
+	vector<string> words;
+	vector<cppjieba::Word> jiebawords;
+	string s = ch0;
+	string result;
+	cout << "切分" << endl;
+	jieba.Cut(s, words, true);
+	
+
+	for (vector<string>::iterator vit = words.begin(); vit != words.end(); vit++) {
+		cout << *vit << " ";
+	}
+
+	//ofstream file("result.txt");
+	//file << limonp::Join(words.begin(), words.end(), "/") << endl;
+	//file.close();
+}
 
 int main() {
 	//test61();
 	//test62();
 	//test63();
-	test65();
+	//test65();
+	test66();
+
+	cout << "你好" << endl;
 
 	system("pause");
 	return 0;
