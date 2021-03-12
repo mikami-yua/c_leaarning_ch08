41e0df0af5756e7cfb58efe9686b010817f1d485
mikami-yua <lovejiaxi_1@163.com>
2021-03-12 10:02:10 +0800

    增加使用停用词的功能

--- 06vector存放内置数据类型.cpp
+++ 06vector存放内置数据类型.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #include<string>
 #include<fstream>
 #include "cppjieba/Jieba.hpp"
-
+#include"cppjieba/KeywordExtractor.hpp"
 
 void myPrintVectotr(int val) {
 	cout << val << endl;
@@ -150,6 +150,7 @@ void test66() {
 	const char* const USER_DICT_PATH = "dict/user.dict.utf8";
 	const char* const IDF_PATH = "dict/idf.utf8";
 	const char* const STOP_WORD_PATH = "dict/stop_words.utf8";
+	char ch1[] = { "报告提出,今年国内生产总值预期增长6%以上。设定这一预期目标，是考虑了经济运行恢复情况，有利于引导各方面集中精力推进改革创新、推动高质量发展。我国2019年预期目标增长6% —6.5 % ，2020年为应对突如其来的新冠肺炎疫情，引导各方面集中精力抓好“六稳”“六保”，没有提出GDP增速量化目标，但依然实现了2.3 % 的增长，是全球唯一实现正增长的主要经济体。这为今年的发展奠定了良好基础。同时，我国超大规模经济体优势决定了经济发展具有较强的韧性和后劲，且2020年基数相对较低，因而有实力、有底气实现今年GDP增长6 % 以上的目标。GDP预期目标，也体现了保持经济运行在合理区间的要求，与今后目标平稳衔接，有利于实现可持续健康发展。6 % 以上，既有基于抓好“六稳”“六保”的底线要求，又蕴含高质量发展的内在要求，就是既要保持一定的增速，又不将速度放在第一位，更重要的是提升增长质量，有效防范化解各类风险隐患，实现更高质量、更有效率、更可持续、更为安全的发展。a" };
 	cout << "加载dict" << endl;
 	cppjieba::Jieba jieba(DICT_PATH,
 		HMM_PATH,
@@ -157,13 +158,9 @@ void test66() {
 		IDF_PATH,
 		STOP_WORD_PATH);
 	vector<string> words;
-	vector<cppjieba::Word> jiebawords;
-	string s = ch0;
-	string result;
+	string s = ch1;
 	cout << "切分" << endl;
 	jieba.Cut(s, words, true);
-	
-
 	for (vector<string>::iterator vit = words.begin(); vit != words.end(); vit++) {
 		cout << *vit << " ";
 	}
@@ -172,15 +169,35 @@ void test66() {
 	//file << limonp::Join(words.begin(), words.end(), "/") << endl;
 	//file.close();
 }
+void test67() {
+	char ch0[] = { "我喜欢吃苹果和apple banana" };
+	char ch1[] = { "报告提出,今年国内生产总值预期增长6%以上。设定这一预期目标，是考虑了经济运行恢复情况，有利于引导各方面集中精力推进改革创新、推动高质量发展。我国2019年预期目标增长6% —6.5 % ，2020年为应对突如其来的新冠肺炎疫情，引导各方面集中精力抓好“六稳”“六保”，没有提出GDP增速量化目标，但依然实现了2.3 % 的增长，是全球唯一实现正增长的主要经济体。这为今年的发展奠定了良好基础。同时，我国超大规模经济体优势决定了经济发展具有较强的韧性和后劲，且2020年基数相对较低，因而有实力、有底气实现今年GDP增长6 % 以上的目标。GDP预期目标，也体现了保持经济运行在合理区间的要求，与今后目标平稳衔接，有利于实现可持续健康发展。6 % 以上，既有基于抓好“六稳”“六保”的底线要求，又蕴含高质量发展的内在要求，就是既要保持一定的增速，又不将速度放在第一位，更重要的是提升增长质量，有效防范化解各类风险隐患，实现更高质量、更有效率、更可持续、更为安全的发展。a" };
+	const char* const DICT_PATH = "dict/jieba.dict.utf8";
+	const char* const HMM_PATH = "dict/hmm_model.utf8";
+	const char* const USER_DICT_PATH = "dict/user.dict.utf8";
+	const char* const IDF_PATH = "dict/idf.utf8";
+	const char* const STOP_WORD_PATH = "dict/stop_words.utf8";
+	cout << "加载dict" << endl;
+	cppjieba::KeywordExtractor we(DICT_PATH, HMM_PATH, IDF_PATH, STOP_WORD_PATH);
+	vector<string> words;
+	string s = ch1;
+	cout << "切分" << endl;
+	size_t size = 250;
+	we.Extract(s,words,size);
+	for (vector<string>::iterator vit = words.begin(); vit != words.end(); vit++) {
+		cout << *vit << " ";
+	}
+}
+
 
 int main() {
 	//test61();
 	//test62();
 	//test63();
 	//test65();
-	test66();
+	test67();
 
-	cout << "你好" << endl;
+	
 
 	system("pause");
 	return 0;
